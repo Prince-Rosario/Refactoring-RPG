9f0e36e445642a6cc949b500c63c7917688aac13
Prince-Rosario <princerosario2003@gmail.com>
2025-05-20 18:08:50 +0700

    more Refactoring

--- src/main/java/ru/hits/CheckStatusAction.java
+++ src/main/java/ru/hits/CheckStatusAction.java
@@ -4,9 +4,9 @@ import static ru.hits.Main.*;
 
 public class CheckStatusAction implements IAction {
     @Override
-    public void handleAction(GameState state) {
+    public GameState handleAction(GameState state) {
         System.out.println("HP: " + state.playerHp() + "/" + state.playerMaxHp() + ", Level: " + state.playerLevel() + ", EXP: " + state.playerExp());
         System.out.println("Inventory: " + inventory);
-
+        return state;
     }
 }
